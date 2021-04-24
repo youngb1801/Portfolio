e6ce801589b73e45e345cdb42ca7a831df4cc937
Bret <bayoung1801@gmail.com>
2021-04-24 15:52:31 -0400

    Update Drug_Review_Classification.py
    added additional steps to py file

--- Review_Classification/Drug_Review_Classification.py
+++ Review_Classification/Drug_Review_Classification.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix
 
 
 # load data
@@ -55,29 +56,94 @@ y_train = y_train.numpy() # tf.keras.utils.to_categorical(y_train, num_classes =
 y_test = y_test.numpy() # tf.keras.utils.to_categorical(y_test, num_classes = (len(condition_counts) + 1), dtype = 'float32')
 
 # vectorize text
-VOCAB_SIZE = 1000
+vocab_size = 1000
 encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(
-    max_tokens=VOCAB_SIZE)
+    max_tokens = vocab_size)
 x_train = encoder.adapt(x_train.batch(64))
 x_test = encoder.adapt(x_test.batch(64))
 
 
 # pad_sequence
-x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen = 500)
-x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen = 500)
+review_len_max = 500
+x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen = review_len_max)
+x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen = review_len_max)
 
 print('Training input shape: ', x_train.shape)
 print('Test input shape: ', x_test.shape)
 
 # classifiying with tf.keras RNN
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Embedding(input_dim = review_len_max, output_dim = 128),
+    tf.keras.layers.LSTM(units = 128),
+    tf.keras.layers.Dense(, activation = 'softmax')
+
+]
+)
+
+# compile model
+model.compile(loss = tf.keras.losses.CategoricalCrossentropy(),
+              optimizer = tf.keras.optimizers.Adam,
+              metrics = ['acc'])
+
+model.summary()
+
+# test model
+history = model.fit(x = x_train, y = y_train,
+        epochs = 10,
+        batch_size = 128,
+        validation_split = 0.25
+    )
 
 # save model
+model.save('drug_review_rnn.h5')
 
-# test models
+# model results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+fig, (ax0, ax1) = plt.subplots(2, 1)
+fig.suptitle('Model Results')
+ax0.plot(epochs, acc, 'bo', label = 'Training Accuracy')
+ax0.plot(epochs, val_acc, 'bo', label = 'Validation Accuracy')
+ax0.set_title('Training and Validation Accuracy')
+ax0.legend()
+
+ax1.plot(epochs, loss, 'bo', label = 'Training Loss')
+ax1.plot(epochs, val_loss, 'bo', label = 'Validation Loss')
+ax1.set_title('Training and Validation Loss')
+ax1.legend()
 
 # model performance
+predictions = model.predict(x_test)
 
 # build confusion matrix
+def plot_cm(y_true, y_pred, figsize = (10,10)):
+    cm = confusion_matrix(y_true, y_pred, labels = np.unique(y_true))
+    cm_sum = np.sum(cm, axis = 1, keepdims = True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index = np.unique(y_true), columns = np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize = figsize)
+    sns.heatmap(cm, cmap= "YlGnBu", annot = annot, fmt = '', ax = ax)
+
+plot_cm(y_test.values.argmax(axis = 1), predictions.values.argmax(axis = 1))
 
 # Accuracy : the proportion of the total number of predictions that were correct.
 # Positive Predictive Value or Precision : the proportion of positive cases that were correctly identified.
